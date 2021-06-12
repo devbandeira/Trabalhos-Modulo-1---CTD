dc4b2ef5c02167c26a2a21687e24f3a44b43fdc6
Alexander <dev.bandeira@outlook.com>
2021-06-11 22:01:25 -0300

    08-06-2021 - T1 Controle de fluxo

---
+++ programacao_imperativa/t4_if_else.js
@@ -0,0 +1,9 @@
+let linguagem = 'jadsaipt';
+// console.log("Estou aprendendo")
+if(linguagem == "javascript")
+{
+    console.log("Estou aprendendo")
+} else
+    {
+        console.log("Aprenderei mais tarde")
+    };
---
+++ programacao_imperativa/t4_primeiro_if.js
@@ -0,0 +1,10 @@
+let status = false;
+
+if (status == 1)
+{
+    console.log("O valor é true - verdadeiro")
+}
+if (status == 0)
+{
+    console.log("O valor é false - falso")
+}
